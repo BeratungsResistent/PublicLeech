2ceda8f894d795f8e3fa0956442915d36285b72a
BeratungsResistent <59779396+BeratungsResistent@users.noreply.github.com>
2020-09-30 16:57:56 +0200

    Update youtube_dl_button.py

--- tobrot/helper_funcs/youtube_dl_button.py
+++ tobrot/helper_funcs/youtube_dl_button.py
@@ -88,8 +88,8 @@ async def youtube_dl_call_back(bot, update):
     if "fulltitle" in response_json:
         description = response_json["fulltitle"][0:1021]
         # escape Markdown and special characters
-    if "description" in response_json:
-        description = response_json["description"][0:1021]
+    #if "description" in response_json:
+        #description = response_json["description"][0:1021]
     LOGGER.info(description)
     #
     tmp_directory_for_each_user = user_working_dir
@@ -143,6 +143,9 @@ async def youtube_dl_call_back(bot, update):
     if "hotstar" in youtube_dl_url:
         command_to_exec.append("--geo-bypass-country")
         command_to_exec.append("IN")
+    if "prosieben" in youtube_dl_url:
+        command_to_exec.append("--geo-bypass-country")
+        command_to_exec.append("DE")
     LOGGER.info(command_to_exec)
     start = datetime.now()
     process = await asyncio.create_subprocess_exec(
